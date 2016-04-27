c4eeac07d14e6af3e020237e5467fc2577d539ac
Alisdair Rankine <info@alisdairrankine.com>
2016-04-27 22:22:34 +0100

    process markdown + readme

--- tharsis.go
+++ tharsis.go
@@ -3,18 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	"github.com/evilspacehamster/tharsis/article"
 )
 
 var paths []string
 
 var root string
 
+var docs map[string]string
+
+var docMutex *sync.RWMutex
+
 func main() {
 	fmt.Println("Tharsis Documentation Service")
 
+	docMutex = &sync.RWMutex{}
 	src := flag.String("src", "", "Source directory for markdown documentation")
 	// out := flag.String("out", "", "HTML Output directory")
 	flag.Parse()
@@ -24,16 +33,27 @@ func main() {
 		return
 	}
 
-	filepath.Walk(*src, visit)
+	filepath.Walk(*src, processFile)
 }
 
-func visit(path string, f os.FileInfo, err error) error {
+func processFile(path string, f os.FileInfo, err error) error {
 
 	p := strings.TrimPrefix(path, root)
 	if strings.HasSuffix(p, ".md") {
 		fmt.Printf("Found: %s\n", p)
 		paths = append(paths, p)
+		go parseMarkdownFile(path, p)
 	}
 
 	return nil
+}
+
+func parseMarkdownFile(filename, relativeName string) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("error reading file '%s': %s\n", filename, err)
+	}
+	docMutex.Lock()
+	defer docMutex.Unlock()
+	docs[relativeName] = article.RenderMarkdownArticle(string(bytes))
 }
